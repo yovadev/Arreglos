c7d04a89bef48c37db57671948b810b1f0f285bd
Yova <10067268+yovadev@users.noreply.github.com>
2018-10-13 10:34:15 -0600

    Initial commit

---
+++ src/arreglos/Arreglos.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arreglos;
+
+/**
+ *
+ * @author Yova
+ */
+public class Arreglos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        /*int x[];
+        x = new int[2];
+        
+        x[0] = 0;
+        x[1] = 1;
+        x[2] = 2;*/
+        
+       int y[] = {1,4,33,31,64,63,77,1,6,3,7,2,73,83,75};
+       
+        for (int i = 0; i < y.length; i++) {
+            System.out.println("El valor es: " + y[i]);
+        }
+        
+    }
+    
+}
